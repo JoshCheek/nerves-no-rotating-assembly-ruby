647d9db311f793635f3c98fb1ef3ba350218627c
Josh Cheek <josh.cheek@gmail.com>
2019-05-01 15:22:46 -0500

    Uhhhhhmmmmmm.... almost!

--- f.rb
+++ f.rb
@@ -11,7 +11,10 @@ class Image < Graphics::Simulation
   end
 
   def draw(n)
-    # rotate(PI/2) * point(1, 0)
+    clear :black
+
+    @start_time ||= Time.now
+    seconds = Time.now - @start_time
 
     num_segments      = 3
     num_rings         = 7
@@ -20,9 +23,16 @@ class Image < Graphics::Simulation
     num_veins         = veins_per_segment * num_segments
     num_dots          = num_veins * num_rings
 
-    time        = 1  # second
+    duration    = 1  # seconds
     radius      = 5  # pixels
-    translation = 10 # pixels
+    vein_radius = radius*8
+    distance    = 10 # pixels
+
+    turn = 2*PI
+
+    deg = lambda do |n|
+      turn * n/360
+    end
 
     dot = lambda do |position|
       circle position[0,0], position[1,0], radius, :white
@@ -37,35 +47,20 @@ class Image < Graphics::Simulation
       vein_in_segment_percent = vein_in_segment_index.to_f / veins_per_segment
       vein_percent            = vein_index.to_f / num_veins
 
-      pt =
-        translate(w/2, h/2) * # move to middle of the screen
-          rotate(
-            ring_index * 2*PI * 5/360 +
-            vein_percent * 2*PI
-          ) *
-          translate(
-            (1+num_rings)*translation +
-              ring_index*translation,
-            0
-          ) *
-          rotate(0) *
-          point(translation, 0)
+      animation_percent = (
+        seconds + (vein_in_segment_percent * -duration)
+      ) % duration
+
+      pt = translate(w/2, h/2) * # move to middle of the screen
+           rotate(ring_index*deg[5] + vein_percent*turn) *
+           translate(vein_radius + ring_index*distance, 0) *
+           rotate(animation_percent*turn) *
+           point(distance, 0)
 
       dot[pt]
     end
 
     # box-shadow: inset 0 0 5px 2px hsl(calc(var(--vein-percent)*360), 100%, 75%);
-    # --pos:
-    # 	rotate(calc(var(--ring-index)*5deg + var(--vein-percent)*1turn))
-    # 	translate(calc(8em + var(--ring-index)*1em));
-    # transform: var(--pos) rotate(0deg) translate($translation);
-    # animation: a $time linear calc(var(--segment-percent)*#{-$time}) infinite
-
-    # @keyframes a {
-    # 	to {
-    # 		transform: var(--pos) rotate(1turn) translate($translation)
-    # 	}
-    # }
   end
 
   new.run
